f5e39406dfad3ceb37305be89ea3514db692d4f3
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-24 16:29:49 -0700

    built first query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,9 @@
+const { User, Thought } = require('../models');
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello World!';
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
         }
     }
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,22 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type Thought{
+    _id:ID
+    thoughtText:String
+    createdAt:String
+    username:String
+    reactionCount:Int
+    reactions:[Reaction]
+}
+type Reaction{
+    _id:ID
+    reactionBody:String
+    createdAt:String
+    username:String
+}
 type Query{
-    hellowWorld:String
+    thoughts(username:String):[Thought]
 }
 `;
 
